a656b55fbd42e6a0040097b641aa490c246bbb3b
git <help.vivat@gmail.com>
2021-01-04 17:19:07 +0200

    commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const jwt = require('jsonwebtoken');
+const jwtSecret = 'ДЖСОНСЕКРЕТОЯЕБУ';
+
+const bcrypt = require('bcrypt');
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('mysql://test@localhost/test');
+
+const getModels = (roles) => {
+    class User extends Model {}
+    User.init(
+        {
+            username: DataTypes.STRING,
+            password: DataTypes.STRING,
+        },
+        { sequelize, modelName: 'user' }
+    );
+};
+// User.hasMany(Post);
+// Post.belongsTo(User);
+
+//console.log(User.prototype)
+//console.log(Post.prototype)
+
+(async () => {
+    await sequelize.sync();
+})();
+
+const { graphqlHTTP: express_graphql } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+    type Query {
+        getUsers: [User]
+        getUser(id: ID!): User
+        login(username: String!, password: String!):String
+    }
+
+    type Mutation {
+        addUser(user: UserInput): User
+        
+    }
+
+    type User {
+        id: ID,
+        username: String,
+        createdAt: String,
+        updatedAt: String
+    }
+
+    input UserInput {
+        username: String!,
+        password: String!,
+    }
+
+`);
+
+var root = {
+    //объект соответствия названий в type Query и type Mutation с функциями-резолверами из JS-кода
+    async getUsers(skip, { user }) {
+        if (!user) throw new Error(`can't get userS when your anon`);
+        return User.findAll({});
+    },
+    async getUser({ id }, { user }) {
+        if (!user) throw new Error(`can't get user when your anon`);
+        return User.findByPk(id);
+    },
+    async addUser({ user: { username, password } }) {
+        password = await bcrypt.hash(password, 10);
+
+        return await User.create({ username, password });
+    },
+
+    async login({ username, password }) {
+        if (username && password) {
+            let user = await User.findOne({ where: { username } });
+            if (user && (await bcrypt.compare(password, user.password))) {
+                const { id } = user;
+                return jwt.sign({ sub: { id, roles: ['user'] } }, jwtSecret);
+            }
+        }
+    },
+};
+
+// Create an express server and a GraphQL endpoint
+app.use(
+    '/graphql',
+    express_graphql(async (req, res) => {
+        let auth = req.headers.authorization;
+        let user, models;
+        if (auth && auth.startsWith('Bearer ')) {
+            let token = auth.slice('Bearer '.length);
+            //console.log('TOKEN FROM REQUEST', token)
+            try {
+                let decoded = jwt.verify(token, jwtSecret);
+                if (decoded) {
+                    user = await User.findByPk(decoded.sub.id);
+                    models = getModels(decoded.sub.roles);
+                    //console.log('НЕ НАЕБАЛ', user)
+                }
+            } catch (e) {
+                console.log('НАЕБАЛ', e);
+            }
+        }
+        return {
+            schema,
+            rootValue: root,
+            graphiql: true,
+            context: { user, models },
+        };
+    })
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
