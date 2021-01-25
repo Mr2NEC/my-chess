d9dfeecfb624d05c63e8fd697b32333676c54681
git <help.vivat@gmail.com>
2021-01-25 17:56:55 +0200

    commit

--- sequelize/action/loginUser.js
+++ sequelize/action/loginUser.js
@@ -1,20 +1,23 @@
-const {jwtSecret} = require("../../defaults.json");
-const {bcrypt} = require("./addUser");
-const {jwt} = require("../../authValidate");
+const { jwtSecret } = require('../../defaults.json');
+const { bcrypt } = require('./addUser');
+const { jwt } = require('../../authValidate');
+const User = require('../schema/userSchema');
 
-const loginUser = async( login, password )=>{
+const loginUser = async (login, password) => {
     if (login && password) {
-        const newPassword = await bcrypt.hash(password, 10);
-        let user = await User.findOne({ where: { login, password:newPassword } });
-        if(!user) throw new Error(`Incorrect login or password`);
-        return {
-            id:user.id,
-            login:user.login,
-            token:jwt.sign({ sub: { id:user.id, login:user.login} },jwtSecret)
-        };
+        if (login && password) {
+            let user = await User.findOne({ where: { login } });
+            if (user && (await bcrypt.compare(password, user.password))) {
+                const { id } = user;
+                return {
+                    id: id,
+                    login: login,
+                    token: jwt.sign({ sub: { id, login } }, jwtSecret),
+                };
+            }
+            throw new Error(`Incorrect login or password`);
+        }
     }
+};
 
-
-}
-
-module.exports = loginUser
+module.exports = loginUser;
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const cors = require('cors');
-const {tokenValidate} = require('./authValidate')
-const {addUser} = require('./sequelize/action/addUser')
-const loginUser = require('./sequelize/action/loginUser')
+const { tokenValidate } = require('./authValidate');
+const { addUser } = require('./sequelize/action/addUser');
+const loginUser = require('./sequelize/action/loginUser');
 
-const {PORT} = require("./defaults.json");
-let usersArr = []
-
+const { PORT } = require('./defaults.json');
+let usersArr = [];
 
 const io = require('socket.io')(server, {
     cors: {
@@ -15,65 +14,70 @@ const io = require('socket.io')(server, {
     },
 });
 
-
-io.on('connection', async (client) => { 
-    let anon = {login: 'anon', id: -1, connectionId:client.id}
+io.on('connection', async (client) => {
+    let anon = { login: 'anon', id: -1, connectionId: client.id };
     usersArr.push(anon);
     console.log(usersArr);
-    client.on('disconnect', ()=>{
-        usersArr = usersArr.filter((user)=>user.connectionId !== client.id );   
-    })
+    client.on('disconnect', () => {
+        usersArr = usersArr.filter((user) => user.connectionId !== client.id);
+    });
 
-    client.on('message', async(data) =>{
-        const user = await tokenValidate(client)
-        if(user.id !== -1){
-            const fUser =  usersArr.find((u)=>{
+    client.on('message', async (data) => {
+        const user = await tokenValidate(client);
+        if (user.id !== -1) {
+            const fUser = usersArr.find((u) => {
                 u.connectionId === client.id;
-            })
-            if(fUser){
-                fUser = {...fUser, ...user}
+            });
+            if (fUser) {
+                fUser = { ...fUser, ...user };
             }
         }
-        try{
+        try {
             switch (data.type) {
-                case "users":
-                    client.send(users)
+                case 'users':
+                    client.send(users);
                     break;
-                case "REGISTER":
-                    await addUser(data.login, data.password)
-                    io.emit(data.type, 200);
+                case 'REGISTER':
+                    await addUser(data.login, data.password);
+                    io.emit('message', {
+                        type: data.type,
+                        payload: 200,
+                    });
                     break;
-                case "LOGIN":
-                    const loggedUser = await loginUser(data.login, data.password)
-                        io.emit(data.type, loggedUser);
-                        usersArr.map(user => {
-                        if(user.connectionId === client.id){
+                case 'LOGIN':
+                    const loggedUser = await loginUser(
+                        data.login,
+                        data.password
+                    );
+                    io.emit('message', {
+                        type: data.type,
+                        ...loginUser,
+                    });
+                    usersArr.map((user) => {
+                        if (user.connectionId === client.id) {
                             user.id = loggedUser.id;
-                            user.login = loggedUser.login
-                        }})
+                            user.login = loggedUser.login;
+                        }
+                    });
                     break;
-                case "LOGOUT":
-                    
+                case 'LOGOUT':
                     break;
-                
+
                 case 'SENDMSG':
                     if (user) {
-
-                    } else error
+                    } else error;
                     break;
                 case msg:
-                    
                     break;
-            
+
                 default:
                     console.log(data.type);
                     break;
             }
-        }catch(e){
-            client.emit(data.type, e.message)
+        } catch (e) {
+            client.emit('message', { type: data.type, message: e.message });
         }
-    })
- });
-
+    });
+});
 
 server.listen(process.env.PORT || PORT);
